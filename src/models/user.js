cfe0f999244dd7844c5f9a34c6092709de8cfa8f
Ana Cruz <anmacruro4@gmail.com>
2023-07-29 21:21:20 -0500

    db

---
+++ src/models/user.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = Schema({
+  user_name: {
+    type: String,
+    required: [true, "El user_name es obligatorio"],
+    unique: true,
+  },
+  password: {
+    type: String,
+  }
+});
+
+UserSchema.methods.toJSON = function () {
+  const { __v, _id, ...rest } = this.toObject();
+  rest.id = _id;
+  return rest;
+};
+
+module.exports = model("user", UserSchema, "users");
